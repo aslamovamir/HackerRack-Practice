fb618c43304ba33512a5999041aef17ab9d0ba08
Amir Aslamov <90361791+aslamovamir@users.noreply.github.com>
2021-11-29 10:48:05 -0500

    Create salesByMatch.cpp

---
+++ salesByMatch.cpp
@@ -0,0 +1,21 @@
+There is a large pile of socks that must be paired by color. Given an array of integers representing the color of each sock, determine how many pairs of socks with matching colors there are.
+Example 
+n = 7;
+ar = [1,2,1,2,1,3,2];
+
+There is one pair of color  and one of color . There are three odd socks left, one of each color. The number of pairs is .
+
+
+int sockMerchant(int n, vector<int> ar) {
+    int answer = 0;
+    map<int , int> SetSocks;
+    for (auto itr: ar) {
+        if (SetSocks[itr] == 0) {
+            SetSocks[itr]++;    
+        } else if (SetSocks[itr] == 1) {
+            answer++;
+            SetSocks[itr] = 0;
+        }
+    }
+    return answer;
+}

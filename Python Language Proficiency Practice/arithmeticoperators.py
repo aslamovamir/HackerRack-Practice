2d3da969339c42110d22f98386541ce8b4d4e81c
Amir Aslamov <90361791+aslamovamir@users.noreply.github.com>
2021-12-16 00:21:03 -0500

    Create arithmeticoperators.py

---
+++ Python Language Proficiency Practice/arithmeticoperators.py
@@ -0,0 +1,19 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+def sum(x, y):
+    sum = x + y
+    return sum
+
+    
+def difference(x, y):
+    dif = x - y
+    return dif
+
+def multiplication(x, y):
+    mult = x * y
+    return mult
+
+    
+print(f"{sum(a, b)}\n{difference(a, b)}\n{multiplication(a, b)}")
